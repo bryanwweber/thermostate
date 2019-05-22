22ef5adc8fa512f41b5a36f2b6102fcd6600a94f
Bryan W. Weber <bryan.w.weber@gmail.com>
2019-05-21 21:04:04 -0400

    Use pathlib and run black on setup.py
    Use pathlib.Path to normalize paths in setup.py Run the black formatter
    on setup.py. Set flake8 line length to match black. Remove pytest
    configuration from setup.cfg

--- setup.py
+++ setup.py
@@ -1,66 +1,53 @@
-from setuptools import setup, find_packages
-import os.path as op
-import sys
+from setuptools import setup
+from pathlib import Path
+from typing import Dict
 
-here = op.abspath(op.dirname(__file__))
+HERE = Path(__file__).parent
 
-version = {}
-with open(op.join(here, 'thermostate', '_version.py'), mode='r') as version_file:
-    exec(version_file.read(), version)
+version: Dict[str, str] = {}
+version_file = HERE / "src" / "thermostate" / "_version.py"
+exec(version_file.read_text(), version)
 
-with open(op.join(here, 'README.md'), mode='r') as readme_file:
-    readme = readme_file.read()
+readme = (HERE / "README.md").read_text()
+changelog = (HERE / "CHANGELOG.md").read_text()
 
-with open(op.join(here, 'CHANGELOG.md'), mode='r') as changelog_file:
-    changelog = changelog_file.read()
+long_description = readme + "\n\n" + changelog
 
-long_description = readme + '\n\n' + changelog
+install_requires = ["coolprop>=6.1.0,<6.3", "pint>=0.7.2,<0.10"]
 
-install_requires = [
-    'coolprop>=6.1.0,<6.3',
-    'pint>=0.7.2,<0.10',
-]
-
-tests_require = [
-    'pytest>=3.0.0',
-    'pytest-cov>=2.3.1',
-]
-
-needs_pytest = {'pytest', 'test', 'ptr'}.intersection(sys.argv)
-setup_requires = ['pytest-runner'] if needs_pytest else []
+tests_require = ["pytest>=3.0.0", "pytest-cov>=2.3.1"]
 
 setup(
-    name='thermostate',
-    version=version['__version__'],
-    description='A package to manage thermodynamic states',
+    name="thermostate",
+    version=version["__version__"],
+    description="A package to manage thermodynamic states",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/bryanwweber/thermostate',
-    author='Bryan W. Weber',
-    author_email='bryan.w.weber@gmail.com',
-    license='BSD-3-clause',
+    long_description_content_type="text/markdown",
+    url="https://github.com/bryanwweber/thermostate",
+    author="Bryan W. Weber",
+    author_email="bryan.w.weber@gmail.com",
+    license="BSD-3-clause",
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Operating System :: MacOS',
-        'Operating System :: MacOS :: MacOS X',
-        'Operating System :: Microsoft',
-        'Operating System :: Microsoft :: Windows',
-        'Operating System :: Microsoft :: Windows :: Windows 10',
-        'Operating System :: Microsoft :: Windows :: Windows 7',
-        'Operating System :: Microsoft :: Windows :: Windows 8',
-        'Operating System :: Microsoft :: Windows :: Windows 8.1',
-        'Operating System :: POSIX',
-        'Operating System :: POSIX :: Linux',
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Operating System :: MacOS",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: Microsoft :: Windows :: Windows 10",
+        "Operating System :: Microsoft :: Windows :: Windows 7",
+        "Operating System :: Microsoft :: Windows :: Windows 8",
+        "Operating System :: Microsoft :: Windows :: Windows 8.1",
+        "Operating System :: POSIX",
+        "Operating System :: POSIX :: Linux",
     ],
     packages=["thermostate"],
     package_dir={"": "src"},
     install_requires=install_requires,
     tests_require=tests_require,
-    setup_requires=setup_requires,
-    python_requires='~=3.5',
+    python_requires="~=3.5",
 )
