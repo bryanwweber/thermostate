c02da90b6a5ad196101da14c5f7d845710b58cc9
Bryan Weber <bryan.w.weber@gmail.com>
2022-03-14 11:42:41 -0400

    Change how versioning of the package is done
    Use VERSION.txt and read that in setup.cfg. This allows us to delete a
    few files. Woo!

--- src/thermostate/__init__.py
+++ src/thermostate/__init__.py
@@ -1,3 +1,9 @@
 from .thermostate import units, Q_, State, set_default_units  # noqa
 from .abbreviations import EnglishEngineering, SystemInternational  # noqa
-from ._version import __version__  # noqa
+
+try:
+    import importlib.metadata as implib
+except ImportError:
+    import importlib_metadata as implib
+
+__version__ = implib.version("thermostate")
